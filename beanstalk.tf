6ea825cac612def8fc2844f3a253cc34da906116
Brad Sickles <brad.sickles@gmail.com>
2023-11-28 15:51:47 -0500

    Added support for configuring beanstalk environment settings from capabilities.

--- beanstalk.tf
+++ beanstalk.tf
@@ -8,6 +8,42 @@ locals {
   // Since some may name their nullstone env "dev", AWS errors when trying to create the beanstalk environment
   // beanstalk_env adds a suffix to ensure the environment name is long enough
   beanstalk_env = "${local.env_name}-${random_string.resource_suffix.result}"
+
+  // Settings Reference: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+  basic_settings = [
+    {
+      namespace = "aws:ec2:vpc"
+      name      = "ELBScheme"
+      value     = "public"
+    },
+    {
+      namespace = "aws:ec2:vpc"
+      name      = "ELBSubnets"
+      value     = join(",", sort(local.public_subnet_ids))
+    },
+    {
+      namespace = "aws:ec2:vpc"
+      name      = "Subnets"
+      value     = join(",", sort(local.private_subnet_ids))
+    },
+    {
+      namespace = "aws:ec2:vpc"
+      name      = "VPCId"
+      value     = local.vpc_id
+    },
+    {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name      = "IamInstanceProfile"
+      value     = aws_iam_instance_profile.this.name
+    },
+    {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name      = "SecurityGroups"
+      value     = join(",", [aws_security_group.this.id])
+    }
+  ]
+  cap_settings = lookup(local.capabilities, "settings", [])
+  all_settings = concat(local.basic_settings, local.cap_settings)
 }
 
 resource "aws_elastic_beanstalk_environment" "this" {
@@ -17,41 +53,13 @@ resource "aws_elastic_beanstalk_environment" "this" {
   solution_stack_name = var.stack
   tier                = "WebServer"
 
-  // Settings Reference: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+  dynamic "setting" {
+    for_each = local.all_settings
 
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "ELBScheme"
-    value     = "public"
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "ELBSubnets"
-    value     = join(",", sort(local.public_subnet_ids))
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.this.name
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "SecurityGroups"
-    value     = join(",", [aws_security_group.this.id])
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = join(",", sort(local.private_subnet_ids))
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = local.vpc_id
+    content {
+      namespace = setting.value.namespace
+      name      = setting.value.name
+      value     = setting.value.value
+    }
   }
 }
--- capabilities.tf
+++ capabilities.tf
@@ -2,6 +2,19 @@
 // This file helps app module creators define a contract for what types of capability outputs are supported.
 locals {
   capabilities = {
+    // settings is a list of settings that are configured on aws_elastic_beanstalk_environment
+    // Reference: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html#command-options-general-ec2instances
+    // These are merged with default settings inside the module to produce a final settings list that is applied to the beanstalk app in this environment
+    // There is currently no logic to prevent capabilities from modifying settings in the app module or other capabilities
+    settings = [
+      {
+        // Namespace describes the area of AWS that this settings is configuring (e.g. "aws:ec2:vpc")
+        namespace = ""
+        name      = ""
+        value     = ""
+      }
+    ]
+
     // private_urls follows a wonky syntax so that we can send all capability outputs into the merge module
     // Terraform requires that all members be of type list(map(any))
     // They will be flattened into list(string) when we output from this module
